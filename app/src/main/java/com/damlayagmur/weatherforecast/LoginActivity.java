ff1cb9ec1582dc95eb0488a8b70288cd71d1de85
damlayagmur <48620858+damlayagmur@users.noreply.github.com>
2020-09-15 22:12:04 +0300

    Authentication

--- app/src/main/java/com/damlayagmur/weatherforecast/LoginActivity.java
+++ app/src/main/java/com/damlayagmur/weatherforecast/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.damlayagmur.weatherforecast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
@@ -7,17 +8,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText enteredEmail;
+    EditText enteredPassword;
+    CardView loginButton;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        enteredEmail = findViewById(R.id.login_page_email);
+        enteredPassword = findViewById(R.id.login_page_password);
+        firebaseAuth = FirebaseAuth.getInstance();
         initViews();
     }
 
@@ -35,8 +51,38 @@ public class LoginActivity extends AppCompatActivity {
         registerPage.setText(registerSpannable);
         registerPage.setMovementMethod(LinkMovementMethod.getInstance());
 
-        CardView loginButton = findViewById(R.id.cardview_activitylogin_loginbutton);
-       // loginButton.setOnClickListener(this);
+
+        loginButton = findViewById(R.id.cardview_activitylogin_loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = enteredEmail.getText().toString();
+                //String name = enteredName.getText().toString();
+                String password = enteredPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    enteredEmail.setError("E-mail is required");
+                } else if (TextUtils.isEmpty(password)) {
+                    enteredPassword.setError("Password is required");
+                } else if (password.length() < 6) {
+                    enteredPassword.setError("Password must be at least 6 character");
+                } else {
+
+
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener <AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task <AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Logged in Succesfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
